63cfe8887d6168263486e5caec295b38eba1e1a5
iLtc <i@iltc.io>
2017-11-05 22:10:52 -0600

    Fix data format problem

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,5 @@
 class EventsController < ApplicationController
   def index
-    events = Event.all
-
-    (0...events.length).each do |index|
-      events[index][:photos] = ActiveSupport::JSON.decode events[index][:photos]
-      events[index][:geo] = ActiveSupport::JSON.decode events[index][:geo]
-      events[index][:categories] = ActiveSupport::JSON.decode events[index][:categories]
-      puts events[index][:categories]
-    end
-
-    @events = events
+    @events = Event.all
   end
 end
--- app/views/events/_event.json.jbuilder
+++ app/views/events/_event.json.jbuilder
@@ -1,1 +1,4 @@
-json.extract! event, :eid, :title, :url, :first_date, :location, :description, :photos, :geo, :categories
+json.extract! event, :eid, :title, :url, :first_date, :location, :description
+json.photos ActiveSupport::JSON.decode event[:photos]
+json.geo ActiveSupport::JSON.decode event[:geo]
+json.categories ActiveSupport::JSON.decode event[:categories]
