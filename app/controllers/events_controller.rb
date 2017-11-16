8120025b183c87c1773c5718cd190977a041db0c
iLtc <i@iltc.io>
2017-11-15 18:12:16 -0600

    Add Event Detail Page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,8 @@ class EventsController < ApplicationController
   def index
     @events = Event.where('first_date > ?', Time.now - 1.hour).order('first_date ASC')
   end
+
+  def detail
+
+  end
 end
