f726f621bcf901ddaf8181b94b2fbc45050bebfe
iLtc <i@iltc.io>
2017-12-04 16:43:50 -0600

    Show Views and Likes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,9 @@ class Event < ApplicationRecord
   has_many :owned_events
 
   attr_accessor :liked, :owned
+
+  def likes
+    # TODO: Find a better way
+    LikedEvent.where('event_id = ?', id).count
+  end
 end
