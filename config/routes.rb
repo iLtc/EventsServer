55a484a79498ef95c1f7c416fd5caf82c58b7ba3
iLtc <i@iltc.io>
2017-12-22 18:45:51 +0800

    Added New Router for Default Event List

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'events#index', defaults: { format: :json }
+
+  get '/events/', to: 'events#index', defaults: { format: :json }
   post '/events/', to: 'events#new', defaults: { format: :json }
   get '/events/categories', to: 'events#categories', defaults: { format: :json }
   get '/events/user', to: 'events#user_events', defaults: { format: :json }
